09141f07cb6c987cefe0082724bb454d479908a7
unknown <therealvredchasing@gmail.com>
2025-07-27 13:41:04 -0400

    update

---
+++ frontend/src/Components/LandingPageContent/LandingPageComponents/LandingPageD.jsx
@@ -0,0 +1,29 @@
+import './LandingPageD.css'
+
+export default function LandingPageD (){
+
+
+
+  return (
+
+    <section className='lp-d-section'>
+      <div className='lp-d-category-wrapper'>
+        <h1 className='lp-d-category'>Explore</h1>
+      </div>  
+      <div className='lp-d-container'>
+        <div className='lp-d-img-container'>
+          <img className='lp-d-img' src='/public/images/CustomHeroImages/monaco.PNG'></img>
+        </div>
+        <div className='lp-d-caption-wrapper'>
+          <h1 className='lp-d-caption-1'>Creator Name</h1>
+        </div>
+      </div>
+    
+    </section>
+
+    
+
+  )
+
+}
+
