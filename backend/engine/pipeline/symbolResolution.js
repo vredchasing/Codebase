315de804e133005592e5f7b7dcfb2bcbbb3a8e89
Abishek Kharel <therealvredchasing@gmail.com>
2026-02-14 01:12:01 -0500

    feat: update astPipeline.js + initalize symbol table

--- backend/engine/pipeline/symbolResolution.js
+++ backend/engine/pipeline/symbolResolution.js
@@ -1,3 +1,7 @@
-export default async function symbolResolver (){
-  
+export default async function symbolResolution (){
+
 };
+
+// helper functions
+
+// pull all the symbols for a project from postgres for hydrating the symbol table cache for the client
